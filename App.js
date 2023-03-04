081274604cfa45a585c1b0c3a95f571196e85c8a
Sebastian <Sebastianorregourrea@gmail.com>
2023-03-04 09:17:57 -0500

    calculadora basica

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container,{flex:1}]}>
+
+      <View style={[styles.container,styles.views,{flex:1, backgroundColor:'powderblue', }]}>
+        <Text>Imagen</Text>
+      </View>
+      
+      <View style={[styles.views,styles.container,{flex:5, backgroundColor:'yellowgreen', }]}>
+      <Text>cuerpo</Text>
+      </View>
+
+      <View style={[styles.container,styles.views,{flex:1, backgroundColor:'gray', }]}>
+      <Text>derechos reservados</Text>
+      </View>
+    
     </View>
   );
 }
 
+//CREAR COMPONENTE PARA LA IMAGEN
+function Banner(){
+  return(
+   <Image source={require('')}>
+
+    </Image>
+
+  );
+}
+
+
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  views:{
+    width:'100%', 
+    height:'100%', 
+    borderColor:'black', 
+    borderWidth:2, 
+    borderRadius:5
+    
+  }
 });
